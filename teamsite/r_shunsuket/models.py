fbd774bb4ed2b1f634ea639728fb46f357a77e9f
shunsuke-t <st.0316@gmail.com>
2018-11-16 19:00:56 +0900

    wip

--- teamsite/r_shunsuket/models.py
+++ teamsite/r_shunsuket/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Post(models.Model):
+    message = models.TextField(max_length=4000)
+    created_at = models.DateTimeField(auto_now_add=True)
+    
